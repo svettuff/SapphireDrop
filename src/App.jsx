4e33f2605298a7c4f304de6f9a308c81dd11d9c7
svettuff <arkadii.nagogaw@gmail.com>
2025-06-09 13:39:05 +0200

    maintain

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import Menu from './Menu'
 import SpinStandard from './SpinStandard'
 import SpinUniqueCollectible from './SpinUniqueCollectible.jsx'
 
+import loadingGif from './gifs/loading.gif';
+
 import './App.css'
 
 function App() {
     const [screen, setScreen] = useState('menu')
     const goMenu = useCallback(() => setScreen('menu'), []);
+    const isMaintaining = true
 
     return (
         <>
+            {isMaintaining && (
+                <div className="maintenance-overlay">
+                    <div className="maintenance-content">
+                        <p>Maintain in progress</p>
+                        <img src={loadingGif} alt="Loading..." />
+                    </div>
+                </div>
+            )}
+
             {screen === 'menu' && (
                 <Menu
                     onStandard={() => setScreen('spinStandard')}
